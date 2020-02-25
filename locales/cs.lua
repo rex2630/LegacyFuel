928009cbf7c3212e6b35283b6f7530c8d8abbd67
rex2630 <rex2630@gmail.com>
2020-02-25 12:40:42 +0100

    Added czech locale

---
+++ locales/cs.lua
@@ -0,0 +1,16 @@
+Locales['cs'] = {
+  ['exit_vehicle'] = 'vystup z vozidla pro natankování',
+  ['refuel_key'] = 'stiskni ~g~E ~w~pro natankování vozidla',
+  ['empty_can'] = 'kanystr je prázdný',
+  ['full_tank'] = 'nádrž je plná',
+  ['buy_can'] = 'stiskni ~g~E ~w~pro zakoupení kanystru za ~g~$',
+  ['cancel_fueling'] = 'stiskni ~g~E~w~ pro zastavení tankování',
+  ['cancel_fueling_can'] = 'stiskni ~g~E~w~ pro zastavení tankování',
+  ['can'] = 'kanystr:',
+  ['car'] = '% | Vozidlo: ',
+  ['no_cash'] = 'nedostatek peněz',
+  ['refuel_can'] = 'stiskni ~g~E ~w~ pro doplneni kanystru',
+  ['no_cash_can'] = 'nedostatek peněz pro naplnění kanystru',
+  ['can_full'] = 'kanystr je plný',
+  ['price'] = 'cena'
+}
